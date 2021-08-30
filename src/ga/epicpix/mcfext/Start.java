a8bb41ad6c1895f2c9dcc75d466c08d1a9797c48
EpicPix <epicpixminecraft@gmail.com>
2021-08-30 21:46:19 +0200

    Add basic file checking

--- src/ga/epicpix/mcfext/Start.java
+++ src/ga/epicpix/mcfext/Start.java
@@ -1,9 +1,28 @@
 package ga.epicpix.mcfext;
 
+import java.io.File;
+
 public class Start {
 
     public static void main(String[] args) {
-        
+        int ret = run(args);
+        if(ret!=0) {
+            System.exit(ret);
+        }
+    }
+
+    public static int run(String[] args) {
+        String compile = String.join(" ", args);
+        if(compile.isEmpty()) {
+            System.err.println("Missing compile path in arguments");
+            return 1;
+        }
+        File file = new File(compile);
+        if(!file.exists()) {
+            System.err.println("File does not exist");
+            return 1;
+        }
+        return 0;
     }
 
 }
