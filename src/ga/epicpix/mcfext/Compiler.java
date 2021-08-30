562ac7537684d7e90865d7bcfcf82bc1f8aa4aea
EpicPix <epicpixminecraft@gmail.com>
2021-08-31 01:12:32 +0200

    Add line compiler

--- src/ga/epicpix/mcfext/Compiler.java
+++ src/ga/epicpix/mcfext/Compiler.java
@@ -3,6 +3,9 @@ package ga.epicpix.mcfext;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Compiler {
 
@@ -16,7 +19,24 @@ public class Compiler {
     }
 
     public static String compile(String data) {
-        return "<TODO>"; //TODO
+        List<String> lines = new ArrayList<>();
+        Collections.addAll(lines, data.split("\n"));
+        lines.removeIf(String::isEmpty);
+
+        ArrayList<String> output = new ArrayList<>();
+
+        for(String line : lines) {
+            String cline = compileLine(line);
+            if (cline != null) {
+                output.add(cline);
+            }
+        }
+
+        return String.join("\n", output.toArray(new String[0]));
+    }
+
+    public static String compileLine(String line) {
+        return line;
     }
 
 }
