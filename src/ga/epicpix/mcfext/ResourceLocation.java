1f4ff2dcc704325fd425d663421de033dd24df2b
EpicPix <epicpixminecraft@gmail.com>
2021-08-31 20:02:12 +0200

    Create ResourceLocation

---
+++ src/ga/epicpix/mcfext/ResourceLocation.java
@@ -0,0 +1,36 @@
+package ga.epicpix.mcfext;
+
+public final class ResourceLocation {
+
+    private final String namespace;
+    private final String location;
+
+    public ResourceLocation(String location) {
+        if(location.contains(":")) {
+            String[] lsplit = location.split(":", 2);
+            this.namespace = lsplit[0];
+            this.location = lsplit[1];
+        }else {
+            this.namespace = "minecraft";
+            this.location = location;
+        }
+    }
+
+    public ResourceLocation(String namespace, String location) {
+        this.namespace = namespace;
+        this.location = location;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getResourceLocation() {
+        return getNamespace() + ":" + getLocation();
+    }
+
+}
